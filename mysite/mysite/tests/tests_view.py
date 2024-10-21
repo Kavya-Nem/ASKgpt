e4d8bb6dd6c314f3a120e254fcb6cc3dd3a3fea1
Kavya-Nem <147103348+Kavya-Nem@users.noreply.github.com>
2024-10-20 19:00:39 -0700

    Update tests_view.py

--- mysite/mysite/tests/tests_view.py
+++ mysite/mysite/tests/tests_view.py
@@ -6,7 +6,6 @@ class TestView(TestCase):
     def test_webpage(self):
         query = "Say this is a test"
         url = reverse("polls:index", kwargs={"query": query})
-        print ("url= " + url)
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
         responseStr = str(response.content)
