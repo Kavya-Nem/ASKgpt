1cdfa0259e13aa045223502a1d8517d18ac4c8f9
Kavya-Nem <147103348+Kavya-Nem@users.noreply.github.com>
2024-10-21 00:32:24 -0700

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY_HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
